5408d28fbc73b05e720a9671885a397b5aad2861
DeyvidAugusto <deyvidaugusto100@gmail.com>
2025-02-17 19:34:27 -0300

    consertando um erro

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
-intents.messages = True
+intents.message_content = True
 bot = commands.Bot(command_prefix='.', intents=intents)
 
 @bot.event
